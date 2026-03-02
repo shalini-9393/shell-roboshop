eb0f92cb1ca2357f24a2789ac0514e460b563ab0
Shalini Sirisha <your-github-email@example.com>
2026-03-02 15:53:36 +0530

    actions

---
+++ catalogue.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERid=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+mkdir -p $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$(basename $0).log"
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+if [ $USERid -ne 0 ]; then
+    echo -e "$R Please run this script as root or with sudo. $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 failed. $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$G $2 successful. $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Disabling NodeJS Module"
+
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20 Module"
+
+dnf install nodejs -y &>> $LOGS_FILE 
+VALIDATE $? "Installing NodeJS"
+
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+VALIDATE $? "Creating system User"
+
+mkdir /app &>> $LOGS_FILE 
+VALIDATE $? "Creating App Directory" 
+
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGS_FILE
+VALIDATE $? "Downloading catalogue code"
+
