f9657551800de7317f6f1a2ad713f03a9d73aff8
Shalini Sirisha <your-github-email@example.com>
2026-03-02 14:40:26 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERid=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+
+if [ $USERid -ne 0 ]; then
+    echo -e "$R Please run this script as root or with sudo. $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 installing failed. $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$G $2 installing successfully. $N" | tee -a $LOGS_FILE
+    fi
+}
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>> $LOGS_FILE
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y &>> $LOGS_FILE
+VALIDATE $? "Installing MongoDB Server"
+
+systemctl enable mongodb &>> $LOGS_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongodb &>> $LOGS_FILE
+VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGS_FILE
+VALIDATE $? "Allowing Remote Connections"
+
+systemctl restart mongodb &>> $LOGS_FILE
+VALIDATE $? "Restarting MongoDB" 
+
