eaabbabf7de685923768c1b59579402b7f39152c
Shalini Sirisha <your-github-email@example.com>
2026-03-04 15:43:32 +0530

    actions

---
+++ mysql.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERid=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+mkdir -p $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$(basename $0).log"
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+if [ $USERid -ne 0 ]; then
+    echo -e "$R Please run this script as root or with sudo. $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 failed. $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$G $2 successful. $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installed MySQL Server"
+
+systemctl enable mysqld
+systemctl start mysqld  
+VALIDATE $? "Enable and Started MySQL"
+
+# get the password from user
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setup root password"
