a58bb5cf9cb88b22e2882d2b68cedd2404365d8f
Shalini Sirisha <your-github-email@example.com>
2026-03-04 17:29:53 +0530

    actions

--- shipping.sh
+++ shipping.sh
@@ -69,9 +69,15 @@ dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>> $LOGS_FILE
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>> $LOGS_FILE
-
+VALIDATE $? "loaded data into MySQL"
+else
+    echo -e "data is already loaded ... $Y SKIPPING $N"
+fi
 systemctl enable shipping 
 systemctl start shipping
 VALIDATE $? "Enabled and started shipping"
