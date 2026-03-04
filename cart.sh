e3413f4bb56863fe25e713a9b4706365d4a3ab96
Shalini Sirisha <your-github-email@example.com>
2026-03-04 15:12:36 +0530

    actions

--- cart.sh
+++ cart.sh
@@ -64,8 +64,8 @@ VALIDATE $? "Installing dependencies"
 cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
 VALIDATE $? "Created systemctl service"
 
-systemctl daemon-reload
-systemctl enable cart
-systemctl start cart
+systemctl daemon-reload &>> $LOGS_FILE
+systemctl enable cart &>> $LOGS_FILE
+systemctl start cart &>> $LOGS_FILE
 VALIDATE $? "Starting cart"
 
