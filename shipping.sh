cd7133a2c084e2773c15300565eeaaff655fd5b0
Shalini Sirisha <your-github-email@example.com>
2026-03-04 16:36:57 +0530

    actions

---
+++ shipping.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+USERID=$(id -u)
+MYSQL_HOST=mysql.shalinidaws88s.online
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+mkdir -p $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$(basename $0).log"
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script as root or with sudo. $N"
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 failed $N"
+        exit 1
+    else
+        echo -e "$G $2 successful $N"
+    fi
+}
+
+dnf install maven -y &>> $LOGS_FILE
+VALIDATE $? "Installed maven"
+
+id roboshop &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system shipping" roboshop
+    VALIDATE $? "Creating system shipping"
+else
+    echo -e "$G shipping roboshop already exists $N"
+fi
+mkdir /app &>> $LOGS_FILE
+VALIDATE $? "Creating App Directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>> $LOGS_FILE
+VALIDATE $? "Downloading shipping"
+
+cd /app &>> $LOGS_FILE
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/* &>> $LOGS_FILE
+VALIDATE $? "Removing existing code"
+
+unzip -o /tmp/shipping.zip &>> $LOGS_FILE
+VALIDATE $? "unzip shipping code"
+
+cd /app 
+mvn clean package &>> $LOGS_FILE
+VALIDATE $? "Installing Building and shipping"
+
+mv target/shipping-1.0.jar shipping.jar &>> $LOGS_FILE
+VALIDATE $? "Moving and renaming shipping"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Created systemctl service"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL"
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+systemctl enable shipping 
+systemctl start shipping
+VALIDATE $? "Enabled and started shipping"
+
+
+
